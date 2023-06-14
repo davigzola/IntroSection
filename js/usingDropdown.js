99b7bb4d1e7e800e89f210c95d6a2b95963f8b78
davigzola <davi.gzola@gmail.com>
2023-06-14 15:14:08 -0300

    Version 0.5

---
+++ js/usingDropdown.js
@@ -0,0 +1,34 @@
+var featuresDropdown = document.querySelector(".features");
+var ulFeatures = document.querySelector(".ul-features");
+
+var companyDropdown = document.querySelector(".company");
+var ulCompany = document.querySelector(".ul-company");
+
+var dropdownArrowFeatures = document.querySelector("#dropdown-arrow-features");
+var dropdownArrowCompany = document.querySelector("#dropdown-arrow-company")
+var dropdownActiveFeatures = false;
+var dropdownActiveCompany = false;
+
+featuresDropdown.addEventListener("click", function(event) {
+    ulFeatures.classList.toggle("hidden");
+
+    dropdownActiveFeatures ^= true;
+
+    if (dropdownActiveFeatures == true) {
+        dropdownArrowFeatures.src ="./img/icon-arrow-up.svg";
+    } else if (dropdownActiveFeatures == false) {
+        dropdownArrowFeatures.src="./img/icon-arrow-down.svg"
+    } 
+})
+
+companyDropdown.addEventListener("click", function() {
+    ulCompany.classList.toggle("hidden");
+
+    dropdownActiveCompany ^= true;
+    
+    if (dropdownActiveCompany == true) {
+        dropdownArrowCompany.src = "./img/icon-arrow-up.svg"
+    } else if(dropdownActiveCompany == false) {
+        dropdownArrowCompany.src = "./img/icon-arrow-down.svg"
+    }
+}) 
